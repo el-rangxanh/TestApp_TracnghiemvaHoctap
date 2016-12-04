1987d9f95ac63953227dcd0e5ab40e781c3879fd
EL <elp.mgodofdeath@gmail.com>
2016-12-04 17:08:46 +0700

    Update QuanLiCauHoi.java

--- app/src/main/java/com/el_rangxanh/test_11/cauhoi/QuanLiCauHoi.java
+++ app/src/main/java/com/el_rangxanh/test_11/cauhoi/QuanLiCauHoi.java
@@ -15,10 +15,108 @@ public class QuanLiCauHoi {
     }
 
     //Lấy danh sách câu hỏi theo dạng mảng
-    public ArrayList laycauhoi(int sttktra, String monhoc) {
+    public ArrayList laycauhoiVatLy(int sttktra, String monhoc) {
         ArrayList<CauHoi> listcauhoi = new ArrayList<CauHoi>();
         SQLiteDatabase db = dbHelper.getReadableDatabase();
         Cursor cursor = db.rawQuery("SELECT * FROM ly1 WHERE sttktra='" + sttktra + "' AND monhoc='" + monhoc + "'", null);
+        cursor.moveToFirst();
+        do {
+            CauHoi dulieucauhoi;
+            dulieucauhoi = new CauHoi(cursor.getInt(0), cursor.getString(1), cursor.getString(2),
+                    cursor.getString(3), cursor.getString(4), cursor.getString(5),
+                    cursor.getString(6), cursor.getInt(7), cursor.getString(8), cursor.getString(9), "");
+            listcauhoi.add(dulieucauhoi);
+        } while (cursor.moveToNext());
+        return listcauhoi;
+    }
+     public ArrayList laycauhoiToan(int sttktra, String monhoc) {
+        ArrayList<CauHoi> listcauhoi = new ArrayList<CauHoi>();
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM toan3 WHERE sttktra='" + sttktra + "' AND monhoc='" + monhoc + "'", null);
+        cursor.moveToFirst();
+        do {
+            CauHoi dulieucauhoi;
+            dulieucauhoi = new CauHoi(cursor.getInt(0), cursor.getString(1), cursor.getString(2),
+                    cursor.getString(3), cursor.getString(4), cursor.getString(5),
+                    cursor.getString(6), cursor.getInt(7), cursor.getString(8), cursor.getString(9), "");
+            listcauhoi.add(dulieucauhoi);
+        } while (cursor.moveToNext());
+        return listcauhoi;
+    }
+     public ArrayList laycauhoiHoaHoc(int sttktra, String monhoc) {
+        ArrayList<CauHoi> listcauhoi = new ArrayList<CauHoi>();
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM hoa1 WHERE sttktra='" + sttktra + "' AND monhoc='" + monhoc + "'", null);
+        cursor.moveToFirst();
+        do {
+            CauHoi dulieucauhoi;
+            dulieucauhoi = new CauHoi(cursor.getInt(0), cursor.getString(1), cursor.getString(2),
+                    cursor.getString(3), cursor.getString(4), cursor.getString(5),
+                    cursor.getString(6), cursor.getInt(7), cursor.getString(8), cursor.getString(9), "");
+            listcauhoi.add(dulieucauhoi);
+        } while (cursor.moveToNext());
+        return listcauhoi;
+    }
+     public ArrayList laycauhoiSinhHoc(int sttktra, String monhoc) {
+        ArrayList<CauHoi> listcauhoi = new ArrayList<CauHoi>();
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM sinh1 WHERE sttktra='" + sttktra + "' AND monhoc='" + monhoc + "'", null);
+        cursor.moveToFirst();
+        do {
+            CauHoi dulieucauhoi;
+            dulieucauhoi = new CauHoi(cursor.getInt(0), cursor.getString(1), cursor.getString(2),
+                    cursor.getString(3), cursor.getString(4), cursor.getString(5),
+                    cursor.getString(6), cursor.getInt(7), cursor.getString(8), cursor.getString(9), "");
+            listcauhoi.add(dulieucauhoi);
+        } while (cursor.moveToNext());
+        return listcauhoi;
+    }
+     public ArrayList laycauhoiTiengAnh(int sttktra, String monhoc) {
+        ArrayList<CauHoi> listcauhoi = new ArrayList<CauHoi>();
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM anh1 WHERE sttktra='" + sttktra + "' AND monhoc='" + monhoc + "'", null);
+        cursor.moveToFirst();
+        do {
+            CauHoi dulieucauhoi;
+            dulieucauhoi = new CauHoi(cursor.getInt(0), cursor.getString(1), cursor.getString(2),
+                    cursor.getString(3), cursor.getString(4), cursor.getString(5),
+                    cursor.getString(6), cursor.getInt(7), cursor.getString(8), cursor.getString(9), "");
+            listcauhoi.add(dulieucauhoi);
+        } while (cursor.moveToNext());
+        return listcauhoi;
+    } 
+    public ArrayList laycauhoiLichSu(int sttktra, String monhoc) {
+        ArrayList<CauHoi> listcauhoi = new ArrayList<CauHoi>();
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM su1 WHERE sttktra='" + sttktra + "' AND monhoc='" + monhoc + "'", null);
+        cursor.moveToFirst();
+        do {
+            CauHoi dulieucauhoi;
+            dulieucauhoi = new CauHoi(cursor.getInt(0), cursor.getString(1), cursor.getString(2),
+                    cursor.getString(3), cursor.getString(4), cursor.getString(5),
+                    cursor.getString(6), cursor.getInt(7), cursor.getString(8), cursor.getString(9), "");
+            listcauhoi.add(dulieucauhoi);
+        } while (cursor.moveToNext());
+        return listcauhoi;
+    }
+    public ArrayList laycauhoiDiaLy(int sttktra, String monhoc) {
+        ArrayList<CauHoi> listcauhoi = new ArrayList<CauHoi>();
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM dia1 WHERE sttktra='" + sttktra + "' AND monhoc='" + monhoc + "'", null);
+        cursor.moveToFirst();
+        do {
+            CauHoi dulieucauhoi;
+            dulieucauhoi = new CauHoi(cursor.getInt(0), cursor.getString(1), cursor.getString(2),
+                    cursor.getString(3), cursor.getString(4), cursor.getString(5),
+                    cursor.getString(6), cursor.getInt(7), cursor.getString(8), cursor.getString(9), "");
+            listcauhoi.add(dulieucauhoi);
+        } while (cursor.moveToNext());
+        return listcauhoi;
+    }
+    public ArrayList laycauhoiCongDan(int sttktra, String monhoc) {
+        ArrayList<CauHoi> listcauhoi = new ArrayList<CauHoi>();
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM congdan1 WHERE sttktra='" + sttktra + "' AND monhoc='" + monhoc + "'", null);
         cursor.moveToFirst();
         do {
             CauHoi dulieucauhoi;
